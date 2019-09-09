e6d0ea6c1f5520180962d22236bef67913253508
izumin5210 <m@izum.in>
2019-09-10 00:53:18 +0900

    Impl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/sh -l
 
-echo "hello $1"
+#!/bin/sh -l
+
+name="$1"
+body="$2"
+path="$3"
+ghr_version="$4"
+
+archive=ghr_${ghr_version}_linux_amd64
+curl -L https://github.com/tcnksm/ghr/releases/download/${ghr_version}/${archive}.tar.gz | tar xzf -
+sudo cp ./${archive}/ghr /usr/local/bin/
+rm -rf ./${archive}
+
+ghr \
+  -username="${GITHUB_ACTOR}" \
+  -repository="${GITHUB_REPOSITORY}" \
+  -token="${GITHUB_TOKEN}" \
+  -commitish="${GITHUB_SHA}" \
+  -name="${name}" \
+  -body="${body}" \
+  ${GITHUB_REF} \
+  ${path}
